b787d65a6f37f8f1693ffa7519beb5b030bd352f
Nicolas Becharat <nicolas.becharat@gmail.com>
2025-02-09 18:01:12 +0100

    feat: sidebar content

---
+++ app/(admin)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+export default function DashboardPage() {
+    return (
+        <div className="flex flex-col items-center justify-center gap-4 p-4 w-full">
+            <h1 className="text-5xl font-bold text-center text-gray-900 dark:text-gray-100 mt-4">
+                Dashboard
+            </h1>
+            <p className="text-gray-600 dark:text-gray-400 text-center text-balance">
+                Dashboard
+            </p>
+        </div>
+    )
+}
