db120f4b945527e95c5ff275a5879521aab306a8
Nicolas Becharat <nicolas.becharat@gmail.com>
2025-02-09 20:23:02 +0100

    fix: type error

--- app/(admin)/dashboard/layout.tsx
+++ app/(admin)/dashboard/layout.tsx
@@ -1,14 +1,11 @@
 import type { Metadata } from "next";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import AppSidebar from "./(_dashboard)/sidebar"
-import HeaderDashboard from "./(_dashboard)/header"
 
 export const metadata: Metadata = {
     title: "Admin",
     description: "Admin",
 };
-
-
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
