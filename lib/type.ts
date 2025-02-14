3085bb47e991b84a33e0936b10cdae6ef0ae9121
Nicolas Becharat <nicolas.becharat@gmail.com>
2025-02-14 10:34:57 +0100

    fix: visitor type & link

--- lib/type.ts
+++ lib/type.ts
@@ -1,4 +1,5 @@
 import { ZodIssue } from "zod";
+import { ColumnDef } from "@tanstack/react-table"
 
 export interface SalonInvitationEmailProps {
     firstName?: string;
@@ -31,3 +32,18 @@ export type FormResponse<T = unknown> = {
     message?: string;
     content?: T;
 };
+
+export interface DataTableProps<TData extends { ticketNumber: string; lastName: string; person: number }, TValue> {
+    columns: ColumnDef<TData, TValue>[];
+    data: TData[];
+}
+
+export type Visitor = {
+    ticketNumber: string
+    lastName: string
+    person: number
+}
+
+export interface ExportExcelProps {
+    dataToExport: { ticketNumber: string, lastName: string, person: number }[]
+}
