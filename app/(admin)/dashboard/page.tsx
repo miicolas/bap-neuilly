1e27e2af0a0302cb79fc271de8bb0e89280206ce
Nicolas Becharat <nicolas.becharat@gmail.com>
2025-02-16 09:11:19 +0100

    style: wrap analystics

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -6,13 +6,15 @@ import { Visitor } from "@/lib/type"
 export default async function DashboardPage() {
 
     const visitors = await ListVisitorsAction()
-    
+
     return (
         <div className="flex flex-col items-center justify-center gap-4 p-4 w-full">
             <h1 className="text-5xl font-bold text-center text-gray-900 dark:text-gray-100 mt-4">
                 Dashboard
             </h1>
-            <InfluenceChart visitors={visitors.content as Visitor[]} />
+            <div className="flex flex-wrap gap-4 w-full">
+                <InfluenceChart visitors={visitors.content as Visitor[]} />
             </div>
+        </div>
     )
 }
